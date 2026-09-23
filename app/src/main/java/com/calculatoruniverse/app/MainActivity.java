ee0e870ab178a592b9f763d61e6509ed90aa7c6f
MASTERMIND <mastermind191209@gmail.com>
2026-09-23 13:59:03 +0600

    Create MainActivity.java

---
+++ app/src/main/java/com/calculatoruniverse/app/MainActivity.java
@@ -0,0 +1,51 @@
+package com.calculatoruniverse.app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        webView.setWebViewClient(
+                new WebViewClient()
+        );
+
+        webView.loadUrl(
+                "https://cracker-birthday-calculator.onrender.com"
+        );
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
